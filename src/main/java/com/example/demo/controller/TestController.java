79f715bc8b4cd51f8559a5d2985a61afe576a955
Shin Yejin <yejin0605@sookmyung.ac.kr>
2023-06-15 18:14:01 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,18 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.service.TodoService;
+
 @RestController
 @RequestMapping("todo")
 public class TestController {
+
 	@Autowired
+
 	private TodoService service;
+
 	@GetMapping("/test")
+
 	public ResponseEntity<?>testTodo(){
+
 		String str = service.testService();
 		List<String> list =new ArrayList<>();
 		list.add(str);
-		ResponseDTO<String>           response 	 = 
+		ResponseDTO<String> response = 
 ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
 	}
 }
+
