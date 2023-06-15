3479aa13f0d4b8a99d61b8d40160c2298ed0d89e
Shin Yejin <yejin0605@sookmyung.ac.kr>
2023-06-15 20:34:25 +0900

    백엔드 개발 18. @NamedQuery

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,23 +1,20 @@
-package com.example.demo.service;
-
+ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
-
 @Service
 public class TodoService {
-	@Autowired	
+	@Autowired
 	private TodoRepository repository;
-	public String testService() {
+	public String testService(){
 		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-		// Todo Entity 저장
+		TodoEntity entity = TodoEntity.builder().userId("Shin Ye Jin").title("My first todo item").build();
+		//Todo Entity 저장
 		repository.save(entity);
-		// Todo Entity 검색
-		TodoEntity   savedEntity = 
-repository.findByUserId(entity.getUserId()).get(0);
+		//Todo Entity 검색
+		TodoEntity savedEntity = 
+				repository.searchByUserId(entity.getUserId()).get(0);
 		return savedEntity.getUserId();
-		}
+	}
 }
